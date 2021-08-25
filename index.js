960c2db6b4fd00c4cee4c2f498207035543aac8e
Anna Malorodova <angervenus6@gmail.com>
2021-08-25 11:11:13 +0300

    Section featured start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-  const swiper = new Swiper('.swiper-container', {
+  const swiperHeader = new Swiper('.header-swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -11,5 +11,18 @@
       type: 'bullets',
       clickable: true,
     }
+  
+  });
+
+  const swiperFeatured = new Swiper('.featured-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 45,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   
   });
